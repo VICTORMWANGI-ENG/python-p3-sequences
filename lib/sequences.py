da0d171a997e9f92a46ff03acb512fd4da15ef60
VICTORMWANGI-ENG <mwangizeus69@gmail.com>
2024-08-30 00:50:51 +0300

    code simplified

--- lib/sequences.py
+++ lib/sequences.py
@@ -4,12 +4,6 @@ def print_fibonacci(length):
     fibonacci_list = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
     if length == 0:
         print([])
-    elif length == 1:
-        print([0]) 
-    elif length == 2:
-        print(fibonacci_list[0:2])
-    elif length == 10:
-        print(fibonacci_list[0:10]) 
     elif  length == length: 
         print(fibonacci_list[0:length])
-print_fibonacci(9)
+print_fibonacci(1)
